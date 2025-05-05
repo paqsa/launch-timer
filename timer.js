da0c1535b022bc7e08df03916544b924cf26cc74
paqsa <gigapaqsa69@gmail.com>
2025-05-05 22:45:54 +0400

    launch js

---
+++ timer.js
@@ -0,0 +1,20 @@
+const countdownDate = new Date("2025-09-31T00:00:00").getTime();
+
+const updateCountdown = () => {
+  const now = new Date().getTime();
+  const distance = countdownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerText = String(days).padStart(2, '0');
+  document.getElementById("hours").innerText = String(hours).padStart(2, '0');
+  document.getElementById("minutes").innerText = String(minutes).padStart(2, '0');
+  document.getElementById("seconds").innerText = String(seconds).padStart(2, '0');
+};
+
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
